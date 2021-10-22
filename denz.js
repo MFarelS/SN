e7b976ba6152fadd427dcf075c667fc3ecfbe2c9
VYNG85 <71924265+vynkevin222@users.noreply.github.com>
2021-10-23 00:18:56 +0700

    Add files via upload

--- denz.js
+++ denz.js
@@ -733,7 +733,8 @@ const fvid = {
                        }
 	                  }
 	                      
-	
+	fkontakk = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6285732415700-1604595598@g.us' } : {})}, message: { "contactMessage":{"displayName": `${from}`,"vcard":`BEGIN:VCARD\nVERSION:3.0\nN:2;Denz;;;\nFN:VIN\nitem1.TEL;waid=6285732415700:6285732415700\nitem1.X-ABLabel:Mobile\nEND:VCARD` }}}
+
 // GROUPINVITE
 const finv = {
 	"key": {
