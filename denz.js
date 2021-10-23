5336e723114ce96cf556923055cf409479380175
VYNG85 <71924265+vynkevin222@users.noreply.github.com>
2021-10-23 19:50:42 +0700

    Add files via upload

--- denz.js
+++ denz.js
@@ -1511,9 +1511,9 @@ case 'bugreport':
               ini_audio = get_result.preview
               jawaban = get_result.judul
               //kisi_kisi = jawaban.replace(/[a|b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z]/gi, '_')
-              teks = jawaban.toLowerCase().replace(/['b','j','g','s','l','m','n','k','t','c','f','r','y','d','w']/g, ' _ ')
+              teks = jawaban.replace(/['b','j','g','s','l','m','n','k','t','c','f','r','y','d','w']/g, ' _ ')
               ini_buffer = await getBuffer(ini_audio)
-              reply('*+* ```Tebak Lagu```\n\n• *Petunjuk* :'+teks+'\n• *Waktu* : 30s')
+              reply('*+* ```Tebak Lagu```\n\n• *Hint* :'+teks+'\n• *Waktu* : 30s')
               //denz.sendMessage(from, ini_buffer, audio, {quoted: mek})
               denz.sendMessage(from, ini_buffer, audio, { mimetype: 'audio/mp4', duration: 359996400, ptt: true, quoted: mek, contextInfo: { forwardingScore: 508, isForwarded: true}}).then(() => {
               tebaklagu[sender.split('@')[0]] = jawaban.toLowerCase()
